e117c5ac65d368bf747c82dea23f52e16ea6f24f
Nina Herrmann <nina.herrmann@viadee.de>
2019-08-06 16:36:24 +0200

    added replace script for generating file adjustments

---
+++ high-level/replace.bat
@@ -0,0 +1,1 @@
+wsl sed -i -e 's/std::pow/__powf/g; s/4242/__brev(x)/g; s/3636/__sinf(temp)/g; s/6363/__cosf(temp)/g' /mnt/c/Users/b98/git/musket-dslbenchmarktaurus/src-gen/pfb64/CUDA/src/pfb64_0.cu
